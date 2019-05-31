2c1f1ab47f920fe5c0a48bdbce32ce3519e2b3a8
HSR <hectorsilvarobles@gmail.com>
2019-05-31 09:56:16 -0700

    just added the database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
+require('dotenv').config()
 const express = require('express')
+const massive = require('massive')
 const app = express()
 
 app.use(express.json())
 
 const controller = require('./controller')
+const {CONNECTION_STRING} = process.env
 var port = 5001
+
+massive(CONNECTION_STRING)
+.then(dbInstance => {
+    app.set('db', dbInstance)
+    console.log('Just connected to Database')
+})
 
 
 app.listen(port, ()=> console.log(`listening on port ${port}`))
